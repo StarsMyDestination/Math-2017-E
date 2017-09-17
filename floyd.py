090393806c78cecaa85f8ded4a2253ec4a52726c
StarsMyDestination <xujianyun101@gmail.com>
2017-09-17 15:24:08 +0800

    rename floyd

---
+++ floyd.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+Nnode = 130
+
+
+def Floyd(Dist):
+    Path = np.zeros((Nnode, Nnode), dtype=np.int16)
+    for i in range(Nnode):
+        for j in range(Nnode):
+            Path[i][j] = i
+    for k in range(Nnode):
+        for i in range(Nnode):
+            for j in range(Nnode):
+                if (Dist[i][k] + Dist[k][j] < Dist[i][j]):
+                    Dist[i][j] = Dist[i][k] + Dist[k][j]
+                    Path[i][j] = Path[k][j]
+    return Dist, Path
